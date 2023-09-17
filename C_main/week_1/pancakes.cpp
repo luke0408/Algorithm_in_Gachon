00ba31d5701f3dd67e8a755ab891d7407c49fd2e
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2023-09-17 21:19:59 +0900

    :sparkles: [Feat]: pancakes and sweet_nap

--- C_main/week_1/pancakes.cpp
+++ C_main/week_1/pancakes.cpp
@@ -2,109 +2,124 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*
-문제: pancakes
-설명:
-- N개의 팬케이크가 쌓여져있을때 해당 팬케이크를 뒤집어서 정렬하고자 한다.
-- flips: 정렬이 아니라 아에 뒤집는 것.
-*/
-
 #define MAX_LENGTH 30
 
-char* get_input();
-int* get_array(char*);
+int *getArray(int *);
+int *toMalloc(int *, int);
 
 void flip(int[], int);
 int findMax(int[], int);
-int pancakeSort(int*, int);
+int pancakeSort(int *, int);
 void printArray(int[], int);
+void printCount();
+
+int global_i = 0;
+int count = 0;
 
 // Driver program to test above function
 int main()
 {
-  int* arr;
-  int n = get_array(get_input(), arr);
+  int *arr = getArray(arr);
 
-  pancakeSort(arr, n);
+  printf("%d\n", global_i);
 
-  puts("Sorted Array ");
-  printArray(arr, n);
+  printf("Sorting starts...\n\n");
+  printArray(arr, global_i);
 
+  pancakeSort(arr, global_i);
+
+  printf(" (");
+  printArray(arr, global_i);
+  printf(")\n");
+  printCount();
   return 0;
 }
 
-char* get_input() {
-  char* intput_str; char input;
-  int i = 0;
+int *getArray(int *arr)
+{
+  int tmp_arr[MAX_LENGTH];
+  int i = 0, n;
 
-  while (scanf("%c", &input) != EOF) {
-    intput_str[i] = input;
-    i++;
+  while (scanf("%d", &n) != EOF)
+  {
+    if (1 <= n && n <= 10)
+      tmp_arr[i++] = n;
+    if (char c = getchar() == '\n')
+      break;
   }
 
-  intput_str[i] = '\0';
+  printf("i: %d\n", i);
+  arr = toMalloc(tmp_arr, i);
 
-  return intput_str;
+  return arr;
 }
 
-int get_array(char* input_str, int* arr) {
-  int temp_arr[MAX_LENGTH];
-  int i = 0;
-
-  char* token = strtok(input_str, " ");
-  int num;
-
-  while (token != NULL && i < MAX_LENGTH) {
-    num = atoi(token);
-
-    if (0 < num && num < 10) {
-      temp_arr[i] = num;
-      i++;
-    }
-
-    token = strtok(NULL, " ");
-  }
-
-  return i;
+int *toMalloc(int tmp_arr[], int i)
+{
+  int *arr = (int *)malloc(sizeof(int) * i);
+  global_i = i;
+  for (int j = 0; j < i; j++)
+    arr[j] = tmp_arr[j];
+  return arr;
 }
 
 /* Reverses arr[0..i] */
-void flip(int arr[], int i) {
+void flip(int *arr, int i)
+{
   int temp, start = 0;
-  
-  while (start < i) {
+
+  while (start < i)
+  {
     temp = arr[start];
     arr[start] = arr[i];
     arr[i] = temp;
     start++;
     i--;
   }
+
+  if (i != 0)
+    count++;
 }
 
-int findMax(int arr[], int n)
+int find_max_index(int *arr, int n)
 {
   int mi, i;
-  
-  for (mi = 0, i = 0; i < n; ++i) {    
-    if (arr[i] > arr[mi]) mi = i;
+
+  for (mi = 0, i = 0; i < n; ++i)
+  {
+    if (arr[i] > arr[mi])
+      mi = i;
   }
-  
+
   return mi;
 }
 
-int pancakeSort(int *arr, int n) {
-  for (int curr_size = n; curr_size > 1; --curr_size) {
-    int mi = findMax(arr, curr_size);
+int pancakeSort(int *arr, int n)
+{
+  for (int curr_size = n; curr_size > 1; --curr_size)
+  {
+    int mi = find_max_index(arr, curr_size);
 
-    if (mi != curr_size - 1) {
+    if (mi != curr_size - 1)
+    {
       flip(arr, mi);
       flip(arr, curr_size - 1);
     }
   }
 }
 
-void printArray(int arr[], int n)
+void printArray(int *arr, int n)
 {
-  for (int i = 0; i < n; ++i)
+  printf("%d ", arr[0]);
+
+  for (int i = 1; i < n; ++i)
     printf("%d ", arr[i]);
+}
+
+void printCount()
+{
+  for (int i = 1; i <= count; i++)
+    printf("%d ", i);
+
+  printf("0\n");
 }
--- C_main/week_1/sweet_nap.cpp
+++ C_main/week_1/sweet_nap.cpp
@@ -1,34 +1,23 @@
-#pragma warning(disable:4996)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/*
-문제: sweet nap
-설명:
-- N개의 일정과 각 일정의 시작 시간과 종료 시간이 주어진다.
-- 이때 가장 길게 낮잠을 잘 수 있는 시간을 출력하라.
-입력:
-- 첫 줄에 일정의 개수 N이 주어진다. (1 <= N <= 100)
-- 이후에 일정에 대한 정보가 주어진다. (ex. hh:mm hh:mm Lecture)
-출력:
-- 가장 길게 낮잠을 잘 수 있는 시작 시간과 이용가능한 시간을 출력한다.
-*/
-
 #define MAX_LENGTH 255
 
-int get_time_gap(char*, char*);
-void input(char*);
+int get_time_gap(char *, char *);
+void input(char *);
 
-int main(){
+int main()
+{
   int n;
   scanf("%d", &n);
 
   int max_gap = 0;
   char result_start[MAX_LENGTH];
-  char cur_end[MAX_LENGTH];  
+  char cur_end[MAX_LENGTH];
 
-  for (int i = 0; i < n; i++){
+  for (int i = 0; i < n; i++)
+  {
     char new_start[MAX_LENGTH];
     char new_end[MAX_LENGTH];
     char new_dis[MAX_LENGTH];
@@ -39,9 +28,11 @@ int main(){
     char stock[MAX_LENGTH];
     strcpy(stock, cur_end);
 
-    if (i > 0) {
+    if (i > 0)
+    {
       int gap = get_time_gap(cur_end, new_start);
-      if (gap > max_gap) {
+      if (gap > max_gap)
+      {
         max_gap = gap;
         strcpy(result_start, stock);
       }
@@ -53,12 +44,13 @@ int main(){
   int h = max_gap / 60;
   int m = max_gap % 60;
 
-  printf("%s %d:%d\n", result_start, h, m);
+  printf("Day #1: the longest nap starts at %s and will last for %d hours and %d minutes.", result_start, h, m);
 
   return 0;
 }
 
-int get_time_gap(char* start, char* end){
+int get_time_gap(char *start, char *end)
+{
   int start_hour = atoi(strtok(start, ":"));
   int start_min = atoi(strtok(NULL, ":"));
   int end_hour = atoi(strtok(end, ":"));
@@ -70,15 +62,17 @@ int get_time_gap(char* start, char* end){
   return end_time - start_time;
 }
 
-void input(char* input){
+void input(char *input)
+{
   int i = 0;
 
   while (i < MAX_LENGTH - 1)
   {
     char c = getchar();
-    if (c == '\n') break;
+    if (c == '\n')
+      break;
     input[i++] = c;
   }
-  
+
   input[i] = '\0';
 }
