a99f674b69fc14b8803a85a27adcfe5cfb8716a9
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2023-11-11 23:33:34 +0900

    ♻️ [Refactor]: refactor rsa code

--- C_main/week_9/rsa.cpp
+++ C_main/week_9/rsa.cpp
@@ -1,92 +1,178 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
 
-int gcd(int, int);
-int rsa(int, int, int);
+/**
+ * [Data Description]
+ * e[100] : Encryption Key
+ * d[100] : Decryption Key
+ * temp[100] : Temporary Key
+ * m[100] : Message
+ * en[100] : Encrypted Message
+ * 
+ * [Variables]
+ * p, q : Prime Number 
+ * n : p * q
+ * t : (p - 1) * (q - 1)
+ * flag : Flag
+ */
+long int p, q, n, t, flag, e[100], d[100], temp[100], j, m[100], en[100], i;
+char msg[100];
 
-int main()
-{
-  // keys
-  int p = 17, q = 13;
-  int n, e, d, f;
+/**
+ * [Methods]
+ */
+int prime(long int);
+void get_ek();
+long int get_dk(long int);
+void encrypt();
+void decrypt();
+
+int main() {
+  printf("\nENTER FIRST PRIME NUMBER: ");
+  scanf("%ld", &p);
+
+  if (!prime(p)) {
+    printf("\nIS NOT PRIME\n");
+    getchar();
+    exit(1);
+  }
+
+  printf("\nENTER ANOTHER PRIME NUMBER: ");
+  scanf("%ld", &q);
+
+  if (!prime(q)) {
+    printf("\nIS NOT PRIME\n");
+    getchar();
+    exit(1);
+  } else if (p == q) {
+    printf("\nBOTH PRIME NUMBERS ARE SAME\n");
+    getchar();
+    exit(1);
+  }
+  
+  printf("\nENTER MESSAGE: ");
+  fflush(stdin);
+  scanf("%s", msg);
+  for (i = 0; msg[i] != '\0'; i++) m[i] = msg[i];
 
   n = p * q;
-  f = (p - 1) * (q - 1);
+  t = (p - 1) * (q - 1);
+  get_ek();
 
-  // find e
-  for (int i = 2; i < f; i++)
-  {
-    if (gcd(p - 1, i) == 1 && gcd(q - 1, i) == 1)
-    {
-      e = i;
-      break;
-    }
-  }
 
-  // find d
-  for (int i = 2; i < n; i++)
-  {
-    if ((1 + f * i) % e == 0)
-    {
-      d = (1 + f * i) / e;
-      break;
-    }
-  }
+  printf("\nPOSSIBLE VALUES OF e AND d List\n");
+  for (i = 0; i < j - 1; i++) printf("\n[%d]\t%ld\t%ld", i, e[i], d[i]);
 
-  printf("p\tq\tn\te\td\tf\n");
-  printf("%d\t%d\t%d\t%d\t%d\t%d\n\n", p, q, n, e, d, f);
+  // print encryption message
+  encrypt();
 
-  // encryption
-  int msg = 'b';
-  printf("msg : %c\n", msg);
-
-  int c = rsa(n, msg, e);
-  printf("c : %c\n", c);
-
-  // decryption
-  int msg2 = rsa(n, c, d);
-  printf("msg : %c\n", msg2);
+  // print decryption message
+  decrypt();
 
   return 0;
 }
 
-int gcd(int a, int b)
-{
-  if (b == 0)
-    return a;
-  return gcd(b, a % b);
+/**
+ * [Prime Number Check]
+ * @param a [long int]
+ * @return  [1 : Prime Number, 0 : Not Prime Number]
+ */
+int prime(long int a) {
+  int k;
+  j = sqrt(a);
+  for (k = 2; k <= j; k++) {
+    if (a % k == 0) return 0;
+  }
+    return 1;
 }
 
-int rsa(int n, int m, int d)
-{
-  int bin[10] = {
-      0,
-  };
-  int tmp, j = 1;
-  unsigned long long x_tmp = 1;
-  int x[10];
-
-  x[0] = (unsigned long long)pow(m, 1) % n;
-  for (int i = 1; i < 10; i++)
-  {
-    x[i] = (x[i - 1] * x[i - 1]) % n;
-  }
-
-  for (int i = 0; d > 0; i++)
-  {
-    tmp = d % 2;
-    d /= 2;
-    bin[i] = tmp;
-  }
-
-  for (int i = 0; i < 10; i++)
-  {
-    if (bin[i] == 1)
-    {
-      x_tmp *= x[i];
+/**
+ * [Encryption Key]
+ * @param   [void]
+ * @return  [void]
+ */
+void get_ek() {
+  int k;
+  k = 0;
+  for (i = 2; i < t; i++) {
+    if (t % i == 0) continue;
+    
+    flag = prime(i);
+    
+    if (flag == 1 && i != p && i != q) {
+      e[k] = i;
+      flag = get_dk(e[k]);
+      if (flag > 0) {
+        d[k] = flag;
+        k++;
+      }
+      if (k == 99) break;
     }
   }
+}
 
-  return x_tmp % n;
+/**
+ * [Decryption Key]
+ * @param x [long int]
+ * @return  [long int]
+ */
+long int get_dk(long int x) {
+  long int k = 1;
+  while (1) {
+    k = k + t;
+    if (k % x == 0) return (k / x);
+  }
+}
+
+/**
+ * [Encryption]
+ * @param   [void]
+ * @return  [void]
+ */
+void encrypt() {
+  long int pt, ct, key = e[0], k, len;
+  i = 0;
+  len = strlen(msg);
+  while (i != len) {
+    pt = m[i];
+    pt = pt - 96;
+    k = 1;
+    for (j = 0; j < key; j++) {
+      k = k * pt;
+      k = k % n;
+    }
+    temp[i] = k;
+    ct = k + 96;
+    en[i] = ct;
+    i++;
+  }
+  en[i] = -1;
+  printf("\n\nTHE ENCRYPTED MESSAGE: ");
+  for (i = 0; en[i] != -1; i++) printf("%c", en[i]);
+}
+
+/**
+ * [Decryption]
+ * @param   [void]
+ * @return  [void]
+ */
+void decrypt() {
+  long int pt, ct, key = d[0], k;
+  i = 0;
+  while (en[i] != -1) {
+    ct = temp[i];
+    k = 1;
+    for (j = 0; j < key; j++) {
+      k = k * ct;
+      k = k % n;
+    }
+    pt = k + 96;
+    m[i] = pt;
+    i++;
+  }
+  m[i] = -1;
+  printf("\n\nTHE DECRYPTED MESSAGE: ");
+  for (i = 0; m[i] != -1; i++) printf("%c", m[i]);
 }
